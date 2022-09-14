4b541320e2dce67c51fbc49beff9451517876a72
omkar <omkardudhewar121@gmail.com>
2022-09-14 13:31:12 +0530

    added method to perform various operations on file IO like deletion,creating directory,etc

---
+++ main/java/com/bridgelabz/FilesUtils.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.io.File;
+
+public class FilesUtils {
+    public static boolean deleteFiles(File contentToDelete) {
+        File[] allContents = contentToDelete.listFiles();
+        if (allContents != null) {
+            for (File eachFile : allContents) {
+                deleteFiles(eachFile);
+            }
+        }
+        return contentToDelete.delete();
+    }
+}
+
